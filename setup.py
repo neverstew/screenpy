2095a19f96466c340063bd3fc4c5c3018962a7a0
mwwsp <matthew.williams@wsp.com>
2018-08-31 16:46:52 +0100

    added dependencies to setup.py and changed structure of Pipfile

--- setup.py
+++ setup.py
@@ -2,5 +2,16 @@ from setuptools import setup, find_packages
 setup(
     name="screenpy",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(
+        include=["screenpy"], exclude=["examples", "tests"]
+    ),
+    install_requires=[
+        "selenium",
+        "pytest",
+        "allure-pytest"
+    ],
+    author="Matthew Williams",
+    author_email="matthew.williams@wsp.com",
+    description="This package provides tools to aid behaviour-driven testing of systems, based around the screenply pattern",
+    license="MIT"
 )
